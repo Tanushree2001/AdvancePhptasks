50204059e509829725fe2407664e12a2777de9c6
tanushree gupta <tanushree.gupta@innoraft.com>
2023-03-02 17:05:39 +0530

    FT2023-73-adding style and comments

--- action.php
+++ action.php
@@ -1,16 +1,18 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\PHPMailer;  //composer
 
-require 'vendor/phpmailer/phpmailer/src/Exception.php';
-require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';  //composer
+require 'vendor/phpmailer/phpmailer/src/SMTP.php'; //composer
 
-class Myclass{
-  private $mail;
+class Myclass{  //created class Myclass
+  private $mail;  //variable created in class 
 
+  //constructor created 
   public function __construct(PHPMailer $mail){
       $this->mail = $mail;
   }
+
+  //function myMethod created
   public function myMethod(){
     $this->mail->isSMTP();  //$this->mailer = SMTP; Send using SMTP
     $this->mail->Host = 'smtp.gmail.com';   //Set the SMTP server to send through
@@ -18,22 +20,21 @@ class Myclass{
     $this->mail->Username = 'tanushree.gupta@innoraft.com'; //SMTP username
     $this->mail->Password = 'zpxqrjrbrfdammao'; //SMTP password
     $this->mail->SMTPSecure = 'tls'; //Enable implicit TLS encryption
-    $this->mail->Port =  587;
+    $this->mail->Port =  587; //port number
 
-    $this->mail->setFrom('tanushree.gupta@innoraft.com');
-    $this->mail->addAddress($_POST['emailid']);
+    $this->mail->setFrom('tanushree.gupta@innoraft.com');  //email sent from this email id
+    $this->mail->addAddress($_POST['emailid']); //email send to
 
     $this->mail->isHTML(true);
-    $this->mail->Body = 'Thank you for your submission';
-
-    $this->mail->send();
-    echo "Sent Successfully";
+    $this->mail->Body = 'Thank you for your submission';  //content of the mail
+    $this->mail->send(); //Function for sending
+    echo "Sent Successfully"; 
   }
 }
 
 if(isset($_POST["submit"])){
-    $mail = new PHPMailer(true);
-    $myobj = new Myclass($mail);
-    $myobj->myMethod(); 
+    $mail = new PHPMailer(true);  
+    $myobj = new Myclass($mail); //object created
+    $myobj->myMethod(); //calling function
 }
 ?>
