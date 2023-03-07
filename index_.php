b515cd162cdda7a43d6c0700300a5cb78428de8e
tanushree gupta <tanushree.gupta@innoraft.com>
2023-03-07 20:37:31 +0530

    FT2023-73-Did advance task 1

--- index_.php
+++ index_.php
@@ -7,6 +7,63 @@
     <title>Document</title>
     <link rel="stylesheet" href="style/style.css">
 </head>
+<style>
+*, body{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+.container{
+  max-width: 1300px;
+  margin: 0 auto;
+}
+.image_block{
+  width: 50%;
+}
+.inner_container{
+  width: 100%;
+  margin: 70px;
+  display: flex;
+}
+.content_block{
+  width: 50%;
+  height: 400px;
+}
+.image{
+  height: 400px;
+  width: auto;
+}
+.logo{
+  width: auto;
+  height: 80px;
+  margin: 15px;
+}
+li{
+  font-family: 'ss-semibold';
+  list-style: none;
+  position: relative;
+  padding-left: 24px;
+  margin: 0 0 20px;
+  line-height: 1.4;
+}
+li a{
+  text-decoration: none;
+  color: #4b5561;
+}
+.service-secondary-title {
+  font-size: 3rem;
+  line-height: 1.4;
+  text-transform: uppercase;
+  font-family: "ss-bold";
+}
+.service-title{
+  font-size: 3rem;
+  line-height: 1.4;
+  text-transform: uppercase;
+  font-family: "ss-bold";
+  color: #ff7900;
+}
+</style>
 <body>
     <section class="container">
     <?php
